e7d155b9ac3d7c230e38fa63c5a26f20fd26c387
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-23 15:52:58 +0700

    #52. Design Homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,6 +1,12 @@
+import videoHomePage from '../../assets/hompagevideo.webm';
+
 const HomePage = () => {
   return (
-    <div className={"homepage-container"}>This is Home page</div>
+    <div className={"homepage-container"}>
+      {/*<video autoPlay muted loop>*/}
+      {/*  <source src={videoHomePage} type="video/webm" />*/}
+      {/*</video>*/}
+    </div>
   );
 };
 
